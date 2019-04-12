246b5fec2ad82e14dd4d5b513e6e04e4b8b0d5bb
Agostina Lo Giudice <agostina.logiudice@gmail.com>
2019-04-12 16:35:35 -0300

    Changed whole structure code; graphics worke better now

--- reflecpy.py
+++ reflecpy.py
@@ -8,112 +8,113 @@ from math import pow
 
 auto_optim = True
 
-def thetacrit(counts, x):
-    # Esta función devuelve el valor del ángulo crítico del sistema, teniendo 
-    # en cuenta a éste calculado como el ángulo en el que I = Imax / 2.
-    max_value = max(counts)
-    max_index = counts.index(max_value)
-    print('Max counts value is: %f' % max_value)
-    thetacrit_value = max_value / 2
-    # Buscamos el valor más cercano al del I/2 y le asignamos el valor de theta
-    # crítico.
-    # scanemos hasta 2 veces el ángulo en el que I = max.
-    maxindex_scan = int(max_index * 1.6)
-    # Croppeamos el array hasta este valor.
-    scan = counts[max_index + 1:maxindex_scan]
+class Reflect():
     
-    for i in range(0, len(scan)):
-        scan[i] = abs(scan[i] - thetacrit_value)    
+    def __init__(self, x, counts):
+        self.x = x
+        self.counts = counts
         
-    mini = np.argmin(scan)
-    thetacritic = x[max_index + 1 + mini]
-    return thetacritic
-
-def get_slope(c, n, fig):
-    # Esta función obtiene los parámetros del ajuste lineal del sin**2 de los 
-    # picos con los valores de n. Toma un array c con las posiciones en 2theta
-    # (en deg) de los picos y el valor n inicial para generar la sucesión de 
-    # de etiquetas.
-    angpeaks = np.zeros(len(c))
-    npeaks = np.arange(n, n + len(c), 1)
-
-    for i in range(0, len(c)):
-        angpeaks[i] = np.sin(pow(c[i] * (np.pi / 360), 2))
-        npeaks[i] = pow(npeaks[i] * lambdax / 2, 2)
-
-    slope, intercept, r_value, p_value, std_err = stats.linregress(npeaks, angpeaks)
-    thetacrit_exp = pow(intercept, 1/2) * (360 / np.pi)
-    thetacrit_diff = ( (thetacrit_exp - thetacrit_int) / thetacrit_int ) * 100
-    #ax[1].clear()
+    def thetacrit(self, maxindex_crop = 1.6):
+        # Esta función devuelve el valor del ángulo crítico del sistema, teniendo 
+        # en cuenta a éste calculado como el ángulo en el que I = Imax / 2.
+        max_value = max(self.counts)
+        max_index = self.counts.index(max_value)
+        thetacrit_value = max_value / 2
+        # Buscamos el valor más cercano al del I/2 y le asignamos el valor de 
+        # theta crítico.
+        maxindex_scan = int(max_index * maxindex_crop)
+        # Croppeamos el array hasta este valor.
+        scan = self.counts[max_index + 1:maxindex_scan]
+        scan = [abs(value - thetacrit_value) for value in scan]
+        mini = np.argmin(scan)
+        thetacritic = x[max_index + 1 + mini]
+        
+        return thetacritic
     
-    #ax[1].draw()
-    ax[1].plot(npeaks, angpeaks, 'o')
-    ax[1].plot(npeaks, npeaks * slope + intercept, color='green')
+    def smoothcounts(self, fraclowess, x_cutoffindex = 1.705):
+        # Esta función grafica los datos sin smoothear, los ya smootheados por 
+        # lowess, y a los picos encontrados. Devuelve el array peaks_index, en el 
+        # que están especificadas las posiciones de los picos en el rango 
+        # [max_index:x_cutoff]
+        
+        # La idea es ver ahora cómo obtener los máx y mín de la curva. Es un proceso
+        # que parece complicado, porque vamos a tener que smoothear la función.
+        # Probé con savgol y no dio muy bien para el tipo de señal que tenemos.
+        # También habría que poner hasta qué pico queremos smoothear.
+        
+        max_value = max(self.counts)
+        max_index = self.counts.index(max_value)
+        x_cutoff = self.x.index(x_cutoffindex)
     
-    print('Fitting results for data:')
-    print("slope: %e,\nIntercept: %e,\nR-squared: %f" % (slope, intercept, r_value))
-    print("2theta_crit value obtained is %f . Value obtained from counts was %f" % (thetacrit_exp, thetacrit_int))
-    print("Percentage difference between both critical angles is %f%%" % thetacrit_diff)
-    return slope, intercept, r_value
-
-
-def smoothcounts(counts, x, fraclowess, x_cutoff, fig):
-    # Esta función grafica los datos sin smoothear, los ya smootheados por 
-    # lowess, y a los picos encontrados. Devuelve el array peaks_index, en el 
-    # que están especificadas las posiciones de los picos en el rango 
-    # [max_index:x_cutoff]
+        # Croppeamos los arrays para realizar los ajustes en este rango.
+        
+        xx = self.x[max_index:x_cutoff]
+        yy = self.counts[max_index:x_cutoff]
     
-    # La idea es ver ahora cómo obtener los máx y mín de la curva. Es un proceso
-    # que parece complicado, porque vamos a tener que smoothear la función.
-    # Probé con savgol y no dio muy bien para el tipo de señal que tenemos.
-    # También habría que poner hasta qué pico queremos smoothear.
+        lowess = sm.nonparametric.lowess(yy, xx, frac=fraclowess)
+        
+        line1, = ax.plot(xx, yy, color='orange', label='data')
+        line2, = ax.plot(xx, lowess[:, 1], color='red', label="Smooth")
+        ax.set_ylabel('counts')
+        ax.set_xlabel('2theta[deg]')
+        ax.set_yscale('log')
     
-    max_value = max(counts)
-    max_index = counts.index(max_value)
+        peaks_index = argrelextrema(lowess[:,1], np.greater)
+        peaks_index_zero = [max_index + y for y in peaks_index[0]]
+        
+        print("Found %d local maxima." % len(peaks_index[0]) )
+        
+        for peak in peaks_index[0]:
+            line3, = ax.plot(xx[peak], lowess[peak,1], color='grey', marker='s')
+            plt.draw()
+        
+        ax.legend(loc='lower left')
+        return peaks_index_zero
 
-    xx = x[max_index:x_cutoff]
-    yy = counts[max_index:x_cutoff]
+
+    def get_slope(self, peaks_index_zero, n):
+        # Esta función obtiene los parámetros del ajuste lineal del sin**2 de los 
+        # picos con los valores de n. Toma un array c con las posiciones en 2theta
+        # (en deg) de los picos y el valor n inicial para generar la sucesión de 
+        # de etiquetas.
+        
+        angpeaks = [0] * len(peaks_index_zero)
+        angpeaks = [np.sin(pow(self.x[peak] * (np.pi / 360), 2)) for peak in peaks_index_zero]
+        npeaks = np.arange(n, n + len(peaks_index_zero), 1)
+        npeaks = [pow(nn * lambdax / 2, 2) for nn in npeaks]
+        
+        
+        slope, intercept, r_value, p_value, std_err = stats.linregress(npeaks, angpeaks)
+        thetacrit_int = self.thetacrit()
+        thetacrit_exp = pow(intercept, 1/2) * (360 / np.pi)
+        thetacrit_diff = ( (thetacrit_exp - thetacrit_int) / thetacrit_int ) * 100
+        
+        fittedline = [m * slope + intercept for m in npeaks]
+        
+        line3, = ax2.plot(npeaks, angpeaks, 'o')
+        line4, = ax2.plot(npeaks, fittedline, color='green')
+    
+        print('Fitting results for data:')
+        print("slope: %e,\nIntercept: %e,\nR-squared: %f" % (slope, intercept, r_value))
+        print("2theta_crit value obtained is %f . Value obtained from counts was %f" % (thetacrit_exp, thetacrit_int))
+        print("Percentage difference between both critical angles is %f%%" % thetacrit_diff)
+        return slope, intercept, r_value
 
     
-    lowess = sm.nonparametric.lowess(yy, xx, frac=fraclowess)
-
-    # Ojo: Estoy casi segura que la frac del lowess deberá cambiarse dependiendo
-    # del caso. Agregar un input para esto.
-    plt.draw()
-    ax[0].scatter(xx, yy, color='orange', label="Data")
-    line1, = ax[0].plot(xx, lowess[:, 1], color='red', label="Smooth")
-    ax[0].set_yscale('log')
-
-    peaks_index = argrelextrema(lowess[:,1], np.greater)
-    print("Found %d local maxima." % len(peaks_index[0]) )
-    c = np.zeros(len(peaks_index[0]))
-
-    for i in range(0, len(peaks_index[0])):
-        k = peaks_index[0][i]
-        c[i] = xx[k]
-        z = lowess[k,1]
-        line2, = ax[0].plot(c[i], z, color='grey', marker='s')
-        plt.draw()
-    
-    ax[0].legend(loc='upper right')
-    ques = input("Counts smoothing is OK? (y/n) (If not, specify a new LOWESS fraction, current is %f)" % fraclowess)
-    return peaks_index, ques, c, xx, yy, lowess
-
-
-def prompt_n(n):
-    # Esta función la definimos para ir cambiando el valor de n según lo que 
-    # decida el usuario.
-    while 1 != 0:
-        prompt = input("Change n value? (Current n is %f):  " % n)
-        if prompt == 'y':
-            n_new = input("Specify new n value:  ")
-            n = float(n_new)
-            break
-        elif prompt == 'n':
-            break
-        else:
-            print('Specify y/n.')
-    return n
+    def prompt_n(n):
+        # Esta función la definimos para ir cambiando el valor de n según lo que 
+        # decida el usuario.
+        while 1 != 0:
+            prompt = input("Change n value? (Current n is %f):  " % n)
+            if prompt == 'y':
+                n_new = input("Specify new n value:  ")
+                n = float(n_new)
+                break
+            elif prompt == 'n':
+                break
+            else:
+                print('Specify y/n.')
+        return n
 
 
 ##############################################################################
@@ -121,18 +122,16 @@ def prompt_n(n):
   ########################### PROGRAM START ###############################
 #plt.use("TkAgg")
 plt.ion()
-fileopen = "XRR_Real1396.dat"
+fileopen = "/home/agostina/git/reflec-py/XRR_Real1396.dat"
 file = open(fileopen, "r")
 x = []
 counts = []
 n = 1
 lambdax = 1.5406 # La longitud de onda de los rayos X usados (Cu)
 
-fig, ax = plt.subplots(2)
-
-ax[0].grid()
-ax[1].grid()
-
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax2 = fig.add_subplot(211)
 
 with open(fileopen) as f:
      data = f.readlines()
@@ -142,57 +141,3 @@ with open(fileopen) as f:
          counts.append(float(line.split(' ')[1]))
          
 file.close()
-
-thetacrit_int = thetacrit(counts, x)
-x_cutoff = x.index(1.705)
-
-
-
-fraclowess = 0.05
-peaks_index, ques, c, xx, yy, lowess = smoothcounts(counts, x, fraclowess, x_cutoff, fig)
-#ax[0].clear()
-#ax[0].grid()
-#ax[0].scatter(xx, yy, color='orange')
-#ax[0].plot(xx, lowess[:, 1], color='red')
-#ax[0].set_yscale('log')
-
-
-promptloop = False
-
-while promptloop == False:
-    if ques == "y":
-        break
-    elif ques == "n":
-        fraclowess = input("Specify a new LOWESS fraction. Current one is %f. : " % fraclowess)
-        peaks_index, ques, c, xx, yy, lowess = smoothcounts(counts, x, float(fraclowess), x_cutoff, fig)
-        ax[0].set_ydata(xx, lowess[:,1], color='red')
-
-
-#Calculamos el valor del theta crítico y lo comparamos con el obtenido anterior
-#mente.
-
-while 1 != 0:
-    n = prompt_n(n)
-    slope, intercept, r_value = get_slope(c, n, fig)
-    a = input("Values OK? (y/n)")
-    if a == 'y':
-        break
-    elif a == 'n':
-        continue
-    else:
-        print('Please answer with y or n.')
-            
-# Calculamos el espesor de la capa especificada.
-width = pow(1 / slope, 1/2)
-width /= 10
-
-print("Layer thickness has been calculated at %f nm." % width)
-
-# plt.scatter(npeaks, angpeaks)
-#plt.ylim(min(angpeaks), max(angpeaks))
-
-
-
-
-
-
